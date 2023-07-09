821c8245446fa87216321a96f4cab255e3eced47
dlehdduq22 <dlehdduq22@naver.com>
2023-07-09 19:51:55 +0900

    User 서비스 클래스

--- main/java/services/UserService.java
+++ main/java/services/UserService.java
@@ -4,4 +4,6 @@ import vo.User;
 
 public interface UserService{
 	public boolean createUser(User user) throws Exception;
+	public boolean idCheckUser(String id) throws Exception;
+	public boolean nameCheckUser(String name) throws Exception;
 }
