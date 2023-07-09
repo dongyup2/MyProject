0682977536307f3cd22357aa6c084d8f9b66637e
dlehdduq22 <dlehdduq22@naver.com>
2023-07-09 19:52:13 +0900

    유저서비스 구현클래스

--- main/java/services/UserServiceImpl.java
+++ main/java/services/UserServiceImpl.java
@@ -13,7 +13,20 @@ public class UserServiceImpl implements UserService{
 	
 	@Override
 	public boolean createUser(User user) throws Exception {
+		 System.out.println("createUser() 메소드가 호출되었습니다. 이름: " + user);
 		return userDao.save(user.toUser()) > 0;
+	}
+
+	@Override
+	public boolean idCheckUser(String id) throws Exception {
+		System.out.println("idCheckUser() 메소드가 호출되었습니다. 아이디: " + id);
+		return userDao.countById(id) > 0;
+	}
+
+	@Override
+	public boolean nameCheckUser(String name) throws Exception {
+		 System.out.println("nameCheckUser() 메소드가 호출되었습니다. 이름: " + name);
+		return userDao.countByName(name) > 0;
 	}
 
 }
