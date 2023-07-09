b295496358f1d8c1bec7e9f207f74271ee061972
dlehdduq22 <dlehdduq22@naver.com>
2023-07-09 19:51:07 +0900

    UserDao 클래스

--- main/java/dao/UserDao.java
+++ main/java/dao/UserDao.java
@@ -4,4 +4,6 @@ import vo.User;
 
 public interface UserDao {
 	public int save(User user) throws Exception;
+	public int countById(String id) throws Exception;
+	public int countByName(String name) throws Exception;
 }
