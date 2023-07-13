c5631078e0b2cda981b3704b14b849d599a2629a
dlehdduq22 <dlehdduq22@naver.com>
2023-07-13 18:26:05 +0900

    모든 유저 정보 vo

--- main/java/vo/UserGameInfo.java
+++ main/java/vo/UserGameInfo.java
@@ -12,31 +12,17 @@ import java.sql.Timestamp;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserGameInfo {
-    private int user_id;
-    private String user_name;
+    // User 클래스의 필드
+    private int mno;
+    private String name;
+    private String id;
+    private String pw;
     private String email;
-    private Timestamp registration_date;
-    private int total_games;
-    private int wins;
-    private int losses;
-    private int draws;
-    private double winning_percentage;
-    private int winning_streak;
-    private int losing_streak;
-
-    public UserGameInfo toUserGameInfo() {
-        return UserGameInfo.builder()
-                .user_id(user_id)
-                .user_name(user_name)
-                .email(email)
-                .registration_date(registration_date)
-                .total_games(total_games)
-                .wins(wins)
-                .losses(losses)
-                .draws(draws)
-                .winning_percentage(winning_percentage)
-                .winning_streak(winning_streak)
-                .losing_streak(losing_streak)
-                .build();
-    }
+    private Timestamp regDate; 
+    // GameRecord 클래스의 필드
+    private int record_id;
+    private int win;
+    private int lose;
+    private int draw;
+    private Timestamp play_date;
 }
