8065a236d4c10cf1eb9527d7a6d12bf7fae26941
dlehdduq22 <dlehdduq22@naver.com>
2023-07-27 20:32:26 +0900

    계속수정중

--- main/java/controller/CreateRoomServlet.java
+++ main/java/controller/CreateRoomServlet.java
@@ -1,11 +1,15 @@
 package controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
 
 import config.ServletContextConfig;
 import services.UserService;
@@ -15,22 +19,44 @@ public class CreateRoomServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	private final UserService userService;
-	
-	public CreateRoomServlet() {
-		userService = ServletContextConfig.getInstance().getUserService();
-	}
+	 private final RoomService roomService; // RoomService 추가
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+	    public CreateRoomServlet() {
+	        userService = ServletContextConfig.getInstance().getUserService();
+	        roomService = new RoomService(); // RoomService 인스턴스 생성
+	    }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	        String roomTitle = request.getParameter("roomTitle");
+	        String password = request.getParameter("password");
+	        String gameType = request.getParameter("gameType");
+
+	        // 처리 결과 변수 초기화
+	        JSONObject result = new JSONObject();
+
+	        try {
+	            // Create room using roomService
+	            int roomId = roomService.createRoom(roomTitle, password, gameType);
+
+	            // 성공적으로 방 생성
+	            result.put("status", "success");
+	            result.put("roomId", roomId);
+	            result.put("roomTitle", roomTitle);
+	            result.put("gameType", gameType);
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	            result.put("status", "fail");
+	            result.put("message", e.getMessage());
+	        }
+
+	        // JSON 형식으로 응답 설정
+	        response.setContentType("application/json");
+	        response.setCharacterEncoding("UTF-8");
+
+	        // JSON 응답 반환
+	        try (PrintWriter out = response.getWriter()) {
+	            out.write(result.toJSONString());
+	        }
+	    }
 
 }
