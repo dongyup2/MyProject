67545143e3f2894b2d90aa46c2e594a47fd10b6c
dlehdduq22 <dlehdduq22@naver.com>
2023-07-27 20:33:20 +0900

    로그아웃 서블릿 수정완료

--- main/java/controller/LogoutServlet.java
+++ main/java/controller/LogoutServlet.java
@@ -8,18 +8,31 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import vo.User;
+
 @WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // 세션 삭제
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logout(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logout(request, response);
+    }
+
+    private void logout(HttpServletRequest request, HttpServletResponse response) throws IOException {
         HttpSession session = request.getSession(false);
-        if (session != null) {
+        if (session != null && session.getAttribute("principal") != null) {
+            String userId = ((User)session.getAttribute("principal")).getId();
             session.invalidate();
-            System.out.println("로그아웃 성공!");
+            System.out.println(userId + "님 로그아웃 성공!");
+        } else {
+            System.out.println("로그인 상태가 아닙니다.");
         }
         // index 페이지로 리다이렉트
-        response.sendRedirect("index");       
+        response.sendRedirect("omokMainPage3.jsp");
     }
 }
