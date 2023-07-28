8e29d6e8c64fd864def93d56cbf3d8859cd1e7b1
dlehdduq22 <dlehdduq22@naver.com>
2023-07-28 20:03:07 +0900

    게임 vo

--- main/java/vo/UserGameInfo.java
+++ main/java/vo/UserGameInfo.java
@@ -1,28 +1,29 @@
 package vo;
 
+import java.sql.Timestamp;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.sql.Timestamp;
-
 @Data
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserGameInfo {
-    // User 클래스의 필드
-    private int mno;
-    private String name;
-    private String id;
-    private String pw;
-    private String email;
-    private Timestamp regDate; 
-    // GameRecord 클래스의 필드
-    private int record_id;
-    private int win;
-    private int lose;
-    private int draw;
-    private Timestamp play_date;
+	private int mno;
+	private String name;
+	private String id;
+	private String pw;
+	private String email;
+	private Timestamp regDate; 
+	
+	private int record_id; //게임 번호
+    private User u_mno; // 유저의 번호
+    private int win; // 승
+    private int lose;// 패
+    private int draw;// 무승부
+    private double odds;//승률
+    private Timestamp play_date;//마지막으로 플레이한 시간
 }
