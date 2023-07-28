ff63cbfd94c6074191996076aec4cc9bd11c9362
dlehdduq22 <dlehdduq22@naver.com>
2023-07-28 20:03:45 +0900

    게임 속성 vo

--- main/java/vo/GameRecord.java
+++ main/java/vo/GameRecord.java
@@ -12,12 +12,13 @@ import java.sql.Timestamp;
 @NoArgsConstructor
 @AllArgsConstructor
 public class GameRecord {
-    private int record_id;
-    private int mno;
-    private int win;
-    private int lose;
-    private int draw;
-    private Timestamp play_date;
+    private int record_id; //게임 번호
+    private int mno; // 유저의 번호
+    private int win; // 승
+    private int lose;// 패
+    private int draw;// 무승부
+    private double odds;//승률
+    private Timestamp play_date;//마지막으로 플레이한 시간
     
     public GameRecord toGameRecord() {
         return GameRecord.builder()
@@ -26,6 +27,7 @@ public class GameRecord {
                 .win(win)
                 .lose(lose)
                 .draw(draw)
+                .odds(odds)
                 .play_date(play_date)
                 .build();
     }
