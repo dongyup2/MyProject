2093460733d62bcd296ad81c211a1bbd5c45450d
dlehdduq22 <dlehdduq22@naver.com>
2023-07-28 20:03:58 +0900

    게임담당 서비스 인터페이스

--- main/java/services/GamePageService.java
+++ main/java/services/GamePageService.java
@@ -1,5 +1,13 @@
 package services;
 
-public interface GamePageService {
+import java.util.List;
 
+import vo.GameRoom;
+
+public interface GamePageService {
+    public int createRoom(String roomTitle, String password, String gameType, String userId);
+    public List<GameRoom> getRoomList();
+    public GameRoom getRoomById(int roomId);
+    public void handleUserJoinOrLeaveRoom(int roomId, String userId, boolean isJoining);
 }
+
