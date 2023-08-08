db43fb1ab6db460e9d49e8f2b289068276be8031
dlehdduq22 <dlehdduq22@naver.com>
2023-08-08 18:38:18 +0900

    커뮤니티 게시판 서비스구현 클래스

---
+++ main/java/services/CommunityBoardServiceImpl.java
@@ -0,0 +1,40 @@
+package services;
+
+import java.util.List;
+
+import vo.Board;
+
+public class CommunityBoardServiceImpl implements CommunityBoardService{
+
+	@Override
+	public boolean insertBoard(Board board) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Board> selectBoard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Board selectOneBoard(int bno) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean updateBoard(Board board) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteBoard(int bno) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+}
