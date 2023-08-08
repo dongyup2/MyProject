6d06303c38bdde8fa5dacc5d83cb21fad2999e8b
dlehdduq22 <dlehdduq22@naver.com>
2023-08-08 18:39:00 +0900

    커뮤니티게시판dao 구현클래스

---
+++ main/java/dao/CommunityBoardDaoImpl.java
@@ -0,0 +1,39 @@
+package dao;
+
+import java.util.List;
+
+import vo.Board;
+
+public class CommunityBoardDaoImpl implements CommunityBoardDao{
+
+	@Override
+	public int insertBoard(Board board) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Board> selectBoard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int updateBoard(Board board) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteBoard(int bno) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Board selectOneBoard(int bno) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
