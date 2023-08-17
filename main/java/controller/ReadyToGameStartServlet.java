62c03aae47bcfebdc09f5810e6291b2d115c6a99
dlehdduq22 <dlehdduq22@naver.com>
2023-08-17 22:27:48 +0900

    게임 준비 시작 서블릿

---
+++ main/java/controller/ReadyToGameStartServlet.java
@@ -0,0 +1,20 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ReadyToGameStartServlet")
+public class ReadyToGameStartServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/plain");
+		response.getWriter().print("true"); // 실제 애플리케이션에 맞게 수정
+	}
+
+}
